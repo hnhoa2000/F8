3a5bc9e6a32ff482b3a6aeea4ba4e41f41c297dc
hnhoa2000 <huynhngochoa2000@gmail.com>
2022-02-17 21:09:46 +0700

    first commit

--- models/config.js
+++ models/config.js
@@ -1,14 +1,6 @@
 const mongose = require('mongoose');
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://hellojoker1001:<Hoapro11>@cluster0.rwwgk.mongodb.net/f8_education_dev?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
+const uri = "mongodb+srv://hellojoker1001:Hoapro11@cluster0.rwwgk.mongodb.net/f8_education_dev?retryWrites=true&w=majority";
 
 async function connect(){
     try {
